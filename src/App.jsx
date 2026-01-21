1f0964b38a6c142579a76620e91cbe7729e33d87
SathvikaSedimbi <sathvikasedimbi@gmail.com>
2026-01-21 08:37:48 +0530

    deployment1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+  return(
+    <div>
+      <div style={{background: "linear-gradient(270deg, #A92655, #FD8D67)" , borderRadius:"30px", width: "100%", height: "500px", marginLeft: "400px"}}>
+        <div style={{paddingTop:"80px"}}><p style={{padding: "50px 50px", textAlign:"center", fontSize:"50px"}}>Hello World</p></div>
+      </div>
+    </div>
+  )
+}
